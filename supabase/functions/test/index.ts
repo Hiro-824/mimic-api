631c66cfa990037955a132fc1560189ccb589587
Hiro-824 <ma8950246@gmail.com>
2024-05-09 19:15:17 +0900

    addressing form file

--- supabase/functions/test/index.ts
+++ supabase/functions/test/index.ts
@@ -1,6 +1,7 @@
 // Setup type definitions for built-in Supabase Runtime APIs
 /// <reference types="https://esm.sh/v135/@supabase/functions-js@2.3.1/src/edge-runtime.d.ts" />
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2.43.1"
+import {multiParser, FormFile} from 'https://deno.land/x/multiparser@0.114.0/mod.ts'
 
 console.log("Hello from Functions!")
 
@@ -13,11 +14,25 @@ Deno.serve(async (req) => {
     { global: { headers: { Authorization: authHeader } } }
   )
 
+  const form = await multiParser(req);
+  console.log("Form data:", form);
+
+  if (!form) {
+    console.log("No file found")
+    return new Response(JSON.stringify({success: false, error: 'no file found'}), {
+      headers: {"Content-Type": "application/json"},
+      status: 400
+    });
+  }
+
+  const audio: FormFile = form.files.file as FormFile;
+  console.log("Audio file:", audio);
+
   const token = authHeader.replace('Bearer ', '')
   const { data: { user } } = await supabaseClient.auth.getUser(token)
 
   const response = {
-    message: `Hello ${user?.id}!`,
+    message: `Hello ${user?.id}! You've sent the file "${audio.contentType}."`,
   }
 
   return new Response(
